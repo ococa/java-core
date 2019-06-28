5cc2f802e290599e2cce268f9ee4105b2736061d
wangchao <wb-wc540140@alibaba-inc.com>
2019-06-28 17:45:21 +0800

    init gitignore

---
+++ FirstSample.java
@@ -0,0 +1,13 @@
+
+// public 是访问修饰符（access modifier）
+// 命名规范 驼峰命名（CamelCase）
+// 每个java程序必须有一个main方法，作为程序的启动点，且main必须是静态
+public class FirstSample {
+    public static void main(String[] args)
+    {
+        double x = 9.997;
+        int ix = (int) x;
+        System.out.println(ix);
+        System.out.print("Hello word\n");
+    }
+}
